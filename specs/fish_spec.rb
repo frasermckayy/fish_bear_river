52d79117a85a982e5a0d3478bf77d80a59756525
Fraser McKay <frasermckay97@gmail.com>
2018-04-03 15:38:03 +0100

    creating fish

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -2,3 +2,13 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../fish.rb")
+
+class FishTest
+
+  def setup()
+    @fish = Fish.new("Wanda", "Flounder", "Speedo", "Goldie")
+  end
+
+
+
+end
