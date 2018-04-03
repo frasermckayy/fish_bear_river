aae884cc8abb6660c8bb884ac9f9b83ff47686cb
Fraser McKay <frasermckay97@gmail.com>
2018-04-03 15:54:37 +0100

    changes to fish

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -2,3 +2,16 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../bear.rb")
+
+class BearTest
+
+  def setup()
+    @bear = Bear.new("Yogi", [])
+  end
+  
+
+
+
+
+
+end
--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -6,9 +6,15 @@ require_relative("../fish.rb")
 class FishTest
 
   def setup()
-    @fish = Fish.new("Wanda", "Flounder", "Speedo", "Goldie")
+    @fish = Fish.new("Wanda")
+    @fish2 = Fish.new("Flounder")
+    @fish3 = Fish.new("Speedo")
+    @fish4 = Fish.new("Goldie")
   end
 
+  # def test_fish_name()
+  #   assert_equal("Flounder", @fish2.fish_name())
+  # end
 
 
 end
